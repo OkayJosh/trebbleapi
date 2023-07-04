bdd2d6fe664a627df6d99080685286dee867fd4e
Joshua Olatunji <joshua@etentlabs.com>
2023-07-04 20:54:23 +0100

    added permissions as model attributes

--- brand/views.py
+++ brand/views.py
@@ -2,12 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 
 from brand.models import Brand, BrandPostTemplate
 from brand.serializers import BrandSerializers, BrandPostTemplateSerializers
-from trebbleapi.mixins import PermissionMixinAdmin
+from trebbleapi.permissions import IsUserOrReadOnly
 
 
 # Create your views here.
-class BrandViewSet(PermissionMixinAdmin, ModelViewSet):
+class BrandViewSet(ModelViewSet):
     serializer_class = BrandSerializers
+    permission_classes = [IsUserOrReadOnly, ]
     filterset_fields = ['uuid']
     lookup_url_kwarg = 'uuid'
 
@@ -15,10 +16,11 @@ class BrandViewSet(PermissionMixinAdmin, ModelViewSet):
         return Brand.objects.order_by('-created')
 
 
-class BrandPostTemplateViewSet(PermissionMixinAdmin, ModelViewSet):
+class BrandPostTemplateViewSet(ModelViewSet):
     serializer_class = BrandPostTemplateSerializers
     filterset_fields = ['uuid']
     lookup_url_kwarg = 'uuid'
+    permission_classes = [IsUserOrReadOnly, ]
 
     def get_queryset(self):
         return BrandPostTemplate.objects.order_by('-created')
