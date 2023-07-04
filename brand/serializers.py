24924d5a83fd3fe093818713d2a2d0cfc5cf7fbf
Joshua Olatunji <joshua@etentlabs.com>
2023-07-04 20:52:15 +0100

    fix references

--- brand/serializers.py
+++ brand/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 
-from brand.models import Brand
+from brand.models import Brand, BrandPostTemplate
 
 
 class BrandSerializers(ModelSerializer):
     class Meta:
         model = Brand
         fields = [
-            'uuid', 'name', 'logo',
-            'descriptions', 'product_description',
+            'name', 'logo',
+            'description', 'product_description',
             'contact_number', 'website_url', 'numbers_of_daily_post',
         ]
 
 
 class BrandPostTemplateSerializers(ModelSerializer):
     class Meta:
-        model = Brand
+        model = BrandPostTemplate
         fields = [
-            'uuid', 'name', 'brand',
+            'name', 'brand',
             'header', 'body', 'footer'
         ]
