c4309efa35bce909aaf07e8952ba457fb6f6f99a
Joshua Olatunji <joshua@etentlabs.com>
2023-07-05 15:28:32 +0100

    get user uuid from request

--- brand/views.py
+++ brand/views.py
@@ -1,3 +1,9 @@
+from copy import copy
+
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from brand.models import Brand, BrandPostTemplate
@@ -8,19 +14,30 @@ from trebbleapi.permissions import IsUserOrReadOnly
 # Create your views here.
 class BrandViewSet(ModelViewSet):
     serializer_class = BrandSerializers
-    permission_classes = [IsUserOrReadOnly, ]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     filterset_fields = ['uuid']
     lookup_url_kwarg = 'uuid'
 
     def get_queryset(self):
         return Brand.objects.order_by('-created')
 
+    def create(self, request, *args, **kwargs):
+        data = copy(request.data)
+        data['user'] = request.user.uuid
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class BrandPostTemplateViewSet(ModelViewSet):
     serializer_class = BrandPostTemplateSerializers
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     filterset_fields = ['uuid']
     lookup_url_kwarg = 'uuid'
-    permission_classes = [IsUserOrReadOnly, ]
 
     def get_queryset(self):
         return BrandPostTemplate.objects.order_by('-created')
